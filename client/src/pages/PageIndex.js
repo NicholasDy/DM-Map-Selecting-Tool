9d471c21a78a920496fde2cc6d786a700feaf440
NicholasDy <77211265+NicholasDy@users.noreply.github.com>
2021-07-08 19:15:48 -0400

    fixed syntax

--- client/src/pages/PageIndex.js
+++ client/src/pages/PageIndex.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
-import Home from './home';
-import Encounter from './encounter';
-import Login from './login';
+import Home from './Home';
+import Encounter from './Encounter';
+import Login from './Login';
 import Nav from '../components/Nav';
 import Footer from '../components/Footer';
-import Upload from './upload';
+import Upload from './Upload';
 import { useAuthContext } from '../components/AuthContext/index';
 
 function PageIndex() {
