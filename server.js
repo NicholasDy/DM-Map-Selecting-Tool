103677085f2c342b061c976b496f79333b68cab2
NicholasDy <77211265+NicholasDy@users.noreply.github.com>
2021-07-08 19:54:33 -0400

    adding build to public folder

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(session(sess));
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "build")));
 app.use(cors());
 
 app.use(routes);
