23dc8fc0eed8ad73cb1feb20d7241e16b5ba8fcb
NicholasDy <77211265+NicholasDy@users.noreply.github.com>
2021-07-08 20:02:13 -0400

    adding a bracket

--- server.js
+++ server.js
@@ -26,6 +26,12 @@ app.use(session(sess));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.static(path.join(__dirname, "build")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  });
+}
 app.use(cors());
 
 app.use(routes);
