cf2028ef016ae29f54d265427f3ba2e4637776e9
NicholasDy <77211265+NicholasDy@users.noreply.github.com>
2021-07-10 12:31:40 -0400

    cleaned up server.js

--- server.js
+++ server.js
@@ -26,15 +26,8 @@ app.use(session(sess));
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("./client/build"));
 app.use(routes);
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("./client/build"));
-  router.use((req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  });
-  app.use(router)
-}
 app.use(cors());
 
 
