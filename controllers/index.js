262e7675b4cf9b6573c401a8da0d9db02058c406
NicholasDy <77211265+NicholasDy@users.noreply.github.com>
2021-07-10 11:48:57 -0400

    trying anything at this point

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
-const {User, Npc, Encounter, Location} = require("../models/index")
+const { User, Npc, Encounter, Location } = require("../models/index")
 const apiRouters = require('./api')
 
 router.use("/api", apiRouters)
-
-
-
+router.use(function (req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, "public")));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+    res.sendFile("index.html");
   });
 }
 app.use(cors());
